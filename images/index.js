fa952b84c1ace1d99aab1ce546d1616f586cab99
xyferr <rohit22153105@akgec.ac.in>
2025-07-01 10:14:06 +0530

    Updates

---
+++ images/index.js
@@ -0,0 +1,7 @@
+
+export const words=[
+  { text: 'Ideas', imgPath: '/images/ideas.svg'},
+  { text: 'Concepts', imgPath: '/images/concepts.svg'},
+  { text: 'Designs', imgPath: '/images/designs.svg'},
+  { text: 'Code', imgPath: '/images/code.svg'},
+]
